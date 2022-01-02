e2dbe4d061874075aa5b845687b3f7b81290e1b2
NevStf <Nevena_98_Cuprija@hotmail.com>
2022-01-02 22:35:11 +0100

    Baza podataka (sa vec napravljenom bazom)

--- model/database.py
+++ model/database.py
@@ -1,13 +1,21 @@
-class Database:
-    username = "username"
-    password = "password"
+# reference: https://www.youtube.com/watch?v=pd-0G0MigUA
+# da bi imali gui za sqlite skinite: https://sqlitebrowser.org/dl/
+import sqlite3
+connection = sqlite3.connect('Database.db')
+cursor = connection.cursor()
 
-    @staticmethod
-    def test_database_connection():
-        return True
-        # something
+# Ovo je zakomentarisano zato sto smo jednom kreirali tabelu, i sad treba samo da je menjamo
+# cursor.execute("""CREATE TABLE Users(
+#             First_Name text,
+#             Last_Name text,
+#             Address text,
+#             City text,
+#             Country text,
+#             Phone_Number text,
+#             Email text,
+#             Password text)""")
 
-    @staticmethod
-    def check_user_exists(email):
-        # something
-        return True
+#cursor.execute("INSERT INTO Users VALUES ('Nevena', 'Stefanovic', 'Adresa 123', 'Grad', 'Drzava', '0642222222', 'email@email.com', 'lozinka123')")
+connection.commit()
+connection.close()
+
