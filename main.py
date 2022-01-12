b00439d1098125588993f060d20bfb5f16712342
Djordje <djordjelazarevic1997@gmail.com>
2022-01-12 16:02:05 +0100

    Login i Registar funkcionalnosti

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
+import sqlite3
 
 app = Flask(__name__)
 
@@ -20,16 +21,53 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+#    if request.method == 'POST':
+#        if request.form['email'] != 'admin@admin.com' or request.form['password'] != 'admin':
+#            error = 'Invalid credentials.'
+#        else:
+#            return redirect(url_for('home'))
     if request.method == 'POST':
-        if request.form['email'] != 'admin@admin.com' or request.form['password'] != 'admin':
-            error = 'Invalid credentials.'
+        email = request.form['email']
+        password = request.form['password']
+        sqlconnection = sqlite3.connect('model/Database.db')
+        cursor = sqlconnection.cursor()
+        query1 = "SELECT Email, Password FROM Users WHERE Email = '{em}' AND Password = '{ps}'".format(
+                                                                                                em=email,
+                                                                                                ps=password)
+        rows = cursor.execute(query1)
+        rows = rows.fetchall()
+        if len(rows) == 1:
+            return render_template("profile.html")
         else:
-            return redirect(url_for('home'))
+            error = 'Invalid credentials.'
     return render_template('login.html', error=error)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        address = request.form['address']
+        city = 0
+        country = request.form['country']
+        phone_number = request.form['phone_number']
+        email = request.form['email']
+        password = request.form['password']
+        sqlconnection = sqlite3.connect('model/Database.db')
+        cursor = sqlconnection.cursor()
+        query1 = "INSERT INTO Users VALUES('{fn}','{ln}','{ad}','{ct}','{cn}','{pn}','{em}','{ps}')".format(
+                                                                                                     fn=firstname,
+                                                                                                     ln=lastname,
+                                                                                                     ad=address,
+                                                                                                     ct=city,
+                                                                                                     cn=country,
+                                                                                                     pn=phone_number,
+                                                                                                     em=email,
+                                                                                                     ps=password)
+        cursor.execute(query1)
+        sqlconnection.commit()
+        return redirect("/")
     return render_template('register.html')
 
 
@@ -40,4 +78,5 @@ def profile():
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
